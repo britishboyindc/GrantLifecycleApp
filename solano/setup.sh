09d20723fadd2d6db69ac81493c9198c4dbc4944
Jason Lantz <jlantz@salesforce.com>
2016-02-04 17:03:33 -0600

    fix path

--- solano/setup.sh
+++ solano/setup.sh
@@ -3,4 +3,4 @@ git clone https://github.com/SalesforceFoundation/CumulusCI
 cd CumulusCI
 git fetch --all
 git checkout features/cloud-ci-integrations
-$CUMULUSCI_PATH/ci/solano/setup.sh
+$CUMULUSCI_PATH/ci/cloud/solano/setup.sh
