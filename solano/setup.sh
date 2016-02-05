d29a3a09f31d2d7b876db67f2707e99531f38a06
Jason Lantz <jlantz@salesforce.com>
2016-02-04 20:31:40 -0600

    Create apex_debug_logs directory on setup

--- solano/setup.sh
+++ solano/setup.sh
@@ -1,6 +1,9 @@
+BASEDIR=`pwd`
 cd /tmp
 git clone https://github.com/SalesforceFoundation/CumulusCI
 cd CumulusCI
 git fetch --all
 git checkout features/cloud-ci-integrations
 $CUMULUSCI_PATH/ci/cloud/solano/setup.sh
+cd $BASEDIR
+mkdir apex_debug_logs
